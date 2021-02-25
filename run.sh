7a46abadc613bb813274a0a0f1ebeca882bfe670
loicttn - pwnh4 <loic.titren@epitech.eu>
2021-02-25 20:31:25 +0100

    azer

--- run.sh
+++ run.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
-./main.py ./data/A.txt > ./results/A.txt
-./main.py ./data/B.txt > ./results/B.txt
-./main.py ./data/C.txt > ./results/C.txt
-./main.py ./data/D.txt > ./results/D.txt
-./main.py ./data/E.txt > ./results/E.txt
-./main.py ./data/F.txt > ./results/F.txt
-
+rm -rf results/*
 zip -r out.zip .
+
+(./main.py ./data/A.txt > ./results/A.txt) &
+(./main.py ./data/B.txt > ./results/B.txt) &
+(./main.py ./data/C.txt > ./results/C.txt) &
+(./main.py ./data/D.txt > ./results/D.txt) &
+(./main.py ./data/E.txt > ./results/E.txt) &
+(./main.py ./data/F.txt > ./results/F.txt) &
+
