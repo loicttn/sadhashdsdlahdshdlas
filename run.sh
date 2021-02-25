46b96792b678d5e8d2fe71e95d5d5f46b0368791
loicttn - pwnh4 <loic.titren@epitech.eu>
2021-02-25 20:11:54 +0100

    cpu à fond

---
+++ run.sh
@@ -0,0 +1,8 @@
+./main.py ./data/A.txt > ./results/A.txt
+./main.py ./data/B.txt > ./results/B.txt
+./main.py ./data/C.txt > ./results/C.txt
+./main.py ./data/D.txt > ./results/D.txt
+./main.py ./data/E.txt > ./results/E.txt
+./main.py ./data/F.txt > ./results/F.txt
+
+zip -r out.zip .
--- tools/solver.py
+++ tools/solver.py
@@ -1,4 +1,6 @@
 from tools.parser import Simulation, Street
+from typing import List
+
 
 class Solver:
     def __init__(self, s: Simulation):
@@ -8,9 +10,14 @@ class Solver:
         print(self.s.inters_nb)
         for inter, streets_indexes in self.s.map.items():
             print(inter)
-            print(len(streets_indexes))
+            streets: List[Street] = []
             for street_index in streets_indexes:
                 street: Street = self.s.streets[street_index]
+                if street.end == inter:
+                    streets.append(street)
+
+            print(len(streets))
+            for street in streets:
                 print(street.name, 1)
             #     print(street.name)
             # print(self.s.map[inter])
